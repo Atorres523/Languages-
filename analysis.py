75f6cb74011eeb16daddf2345b384d16a91535fc
Alexis Torres <alexistor583@gmail.com>
2022-11-07 15:37:26 -0800

    Added analysis.py, a file in which we will all work together on. Also changed README

---
+++ analysis.py
@@ -0,0 +1,27 @@
+"""
+# course: cmps3500
+# CLASS Project
+# PYTHON IMPLEMENTATION: BASIC DATA ANALYSIS
+# date: 09/10/09
+# Student 1: Alexis Torres
+# Student 2: Esmeralda Osornio
+# Student 3: Miguel Bueno Nunez
+# Student 4: Edward Kyles
+# description: Implementation Basic Data Analysys Routines
+"""
+
+# Part 1: Data Loading
+# ***********************************************************
+
+
+# Part 2: Exploring the Data
+# ***********************************************************
+
+# Part 3: Describing the Data
+# ***********************************************************
+
+# Part 4: Analysis
+# ***********************************************************
+
+# Part 5: Interface
+# ***********************************************************
